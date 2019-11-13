581235fcc07032634bb89076eb0ea9a4bba437e7
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-11-13 18:25:39 +0200

    Check if contact already exists before insert

--- src/contact.c
+++ src/contact.c
@@ -13,16 +13,24 @@ void addNewContact()
     fgets(newConctact.Name, sizeof(newConctact.Name), stdin);
     printf("Lastname: ");
     fgets(newConctact.LastName, sizeof(newConctact.LastName), stdin);
-    printf("Phone number: ");
-    fgets(newConctact.PhoneNumber1, sizeof(newConctact.PhoneNumber1), stdin);
-    printf("Mobile phone number: ");
-    fgets(newConctact.PhoneNumber2, sizeof(newConctact.PhoneNumber2), stdin);
-    printf("Email address: ");
-    fgets(newConctact.Email, sizeof(newConctact.Email), stdin);
-    printf("Street address: ");
-    fgets(newConctact.Address, sizeof(newConctact.Address), stdin);
 
-    InsertContact(&newConctact);
+    if(contactExists(&newConctact)) {
+        printf("Contact already exists\n");
+        // TODO: return back to menu
+    }
+    else {
+
+        printf("Phone number: ");
+        fgets(newConctact.PhoneNumber1, sizeof(newConctact.PhoneNumber1), stdin);
+        printf("Mobile phone number: ");
+        fgets(newConctact.PhoneNumber2, sizeof(newConctact.PhoneNumber2), stdin);
+        printf("Email address: ");
+        fgets(newConctact.Email, sizeof(newConctact.Email), stdin);
+        printf("Street address: ");
+        fgets(newConctact.Address, sizeof(newConctact.Address), stdin);
+
+        InsertContact(&newConctact);
+    }
 }
 
 void removeContact()
